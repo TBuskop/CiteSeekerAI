355e5769d37ef3f26c9cc324f55a88d297cedad8
TedB <buskop@hotmail.com>
2025-05-09 11:55:44 +0200

    adjust config

--- config.py
+++ config.py
@@ -11,37 +11,21 @@ load_dotenv(override=True)  # Force override of existing environment variables
 
 # --- Query ---
 QUERY = """
-    find academic peer reviewed literature to support statements in the following text. Give me a list the supported statements and which papers are relevant for it and why
-
-We are approaching a world where society is faced with tough choices due to the effects of climate change. Where we thought that we could adapt to 1.5 degrees warming we find our selves in a situation where global leaders have not taken enough action to limit emissions, meaning we will overshoot the goal set in Paris 2015. At the same time, our adaptation plans to keep society function as it does now reach short of that goal. We reach limits to what we can do to keep things the same. As a consequence we need to find the trade-offs in future actions. What climate do we adapt to, what do we prioritise and what gets left behind?  
-
-One example we detail here is food production in the European Union. The EU has for a long time subsidised farmers within its border in order to guarantee nutritious food production at low consumer prices and secure the livelihoods of those producing this food. Currently a third of the annual EU budget goes towards agricultural policies. One of the biggest agricultural producers and receiver of funds is Spain. Many countries within the EU depend on Spain to produce part of their food supply. 
-
-Now as, the climate changes, Spain is projected to be one of the most water scarce regions in Europe. At the moment many regions in Spain are already water stressed and water use limits are in place. The future could exacerbate the situation leading to competition of water resources among purposes. If no action is taken, this not only threatens local production capacity and livelihoods, but also the food supply to other EU member states. 
-
-Now the question is what do we do? How much less water is available for crop production? What measures and policies can be implemented? What are the trade-ofss between them? Who should fund these
-
-Potential policies could be:
-- increase irrigation from groundwater
-	- Long term depletion of strategic reserves
-	- Many smallholders in Spain probably unable to invest
-- scale down other water users
-	- reduce tourism, industry, domestic use
-- Increase water storage capacity
-	- 
-- Moving production elsewhere
-	- more secure food supply for the European community
-	- Local jobs are lost in a place already affected harshly by climate change
+    What are climate risk storylines and how can they informa climate adaptation plans?
 """
-QUERY_DECOMPOSITION_NR = 5  # Number of sub-queries to generate from the main query
+QUERY_DECOMPOSITION_NR = 3  # Number of sub-queries to generate from the main query
 
 # --- Scopus Search string ---
 SCOPUS_SEARCH_STRING = """
-    ("impact attribution" AND "climate")
+    ("climate storylines")
 """
 
-USE_SCIHUB = True  # Use SciHub for collecting papers
+# --- Query Configuration ---
+# How many results to retrieve at different stages.
+TOP_K_ABSTRACTS = 20  # Number of papers to retrieve based on abstract similarity
+DEFAULT_TOP_K = 30    # Number of chunks to retrieve from the selected papers for context
 
+USE_SCIHUB = False  # Use SciHub for collecting papers
 
 # --- API Keys ---
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
@@ -50,11 +34,6 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 # These are the primary models you'll interact with.
 CHAT_MODEL = "gemini-2.5-flash-preview-04-17" #"gemini-2.0-flash-lite", "gemini-2.5-pro-exp-03-25"  # Main model for chat and generation
 SUBQUERY_MODEL = "gemini-2.5-pro-exp-03-25" # Model for generating sub-queries (more powerful is better and relevant questions)
-
-# --- Query Configuration ---
-# How many results to retrieve at different stages.
-TOP_K_ABSTRACTS = 50  # Number of papers to retrieve based on abstract similarity
-DEFAULT_TOP_K = 20    # Number of chunks to retrieve from the selected papers for context
 
 # --- Chunking Parameters ---
 # Settings for how documents are split into smaller pieces.
