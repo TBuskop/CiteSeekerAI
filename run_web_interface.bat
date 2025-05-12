5fba0b621919c5ee015cb4d097275ab38d7b9909
TedB <buskop@hotmail.com>
2025-05-12 16:58:29 +0200

    increase timeout duration for Flask server startup

--- run_web_interface.bat
+++ run_web_interface.bat
@@ -9,7 +9,7 @@ REM Start the Flask server in a new terminal window
 start "CiteSeekerAI Server" python src/web_interface/app.py
 
 REM Wait a bit to ensure the server has time to start
-timeout /t 3 > nul
+timeout /t 15 > nul
 
 REM Open the web interface in the default browser
 start http://127.0.0.1:5000
